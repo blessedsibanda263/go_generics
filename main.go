76b252755f7001b7148c65c731535030ead2ae20
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-11 13:51:17 +0200

    A filter function with specific types

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 	"unicode"
 )
 
+type Numeric interface {
+	int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func filterPositive[T Numeric](items []T) []T {
+	var filtered []T
+	for _, v := range items {
+		if v > 0 {
+			filtered = append(filtered, v)
+		}
+	}
+	return filtered
+}
+
 func filter[T any](items []T, fx func(T) bool) []T {
 	var filtered []T
 	for _, v := range items {
@@ -31,4 +45,12 @@ func main() {
 		return false
 	})
 	fmt.Println(ints)
+
+	ints2 := []int8{-4, -3, -2, -1, 0, 1, 2, 3, 4}
+	ints2 = filterPositive(ints2)
+	fmt.Println(ints2)
+
+	floats := []float32{-4.5, -3.5, -2.5, -1.5, 0, 0.5, 1.5, 2.5, 3.5, 4.5}
+	floats = filterPositive(floats)
+	fmt.Println(floats)
 }
